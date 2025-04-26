fde4ecffedaeed8d2d08b7f67c3a7b279a9e3c1f
Artem Zakharchenko <kettanaito@gmail.com>
2025-04-26 09:15:48 +0200

    chore: no need to use `.emitAsPromise`

--- test/event-abort.test.ts
+++ test/event-abort.test.ts
@@ -1,93 +1,93 @@
 import { Emitter } from '../src'
 
-it('(on) supports aborting a listener by calling `abort()` on its controller', async () => {
+it('(on) supports aborting a listener by calling `abort()` on its controller', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = emitter.on('greeting', listener)
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
 
-it('(on) supports aborting a listener by providing it a custom `AbortController`', async () => {
+it('(on) supports aborting a listener by providing it a custom `AbortController`', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = new AbortController()
   emitter.on('greeting', listener, { signal: controller.signal })
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
 
-it('(once) supports aborting a listener by calling `abort()` on its controller', async () => {
+it('(once) supports aborting a listener by calling `abort()` on its controller', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = emitter.once('greeting', listener)
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
 
-it('(once) supports aborting a listener by providing it a custom `AbortController`', async () => {
+it('(once) supports aborting a listener by providing it a custom `AbortController`', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = new AbortController()
   emitter.once('greeting', listener, { signal: controller.signal })
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
 
-it('(earlyOn) supports aborting a listener by calling `abort()` on its controller', async () => {
+it('(earlyOn) supports aborting a listener by calling `abort()` on its controller', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = emitter.earlyOn('greeting', listener)
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
 
-it('(earlyOn) supports aborting a listener by providing it a custom `AbortController`', async () => {
+it('(earlyOn) supports aborting a listener by providing it a custom `AbortController`', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = new AbortController()
   emitter.earlyOn('greeting', listener, { signal: controller.signal })
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
 
-it('(earlyOnce) supports aborting a listener by calling `abort()` on its controller', async () => {
+it('(earlyOnce) supports aborting a listener by calling `abort()` on its controller', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = emitter.earlyOnce('greeting', listener)
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
 
-it('(earlyOnce) supports aborting a listener by providing it a custom `AbortController`', async () => {
+it('(earlyOnce) supports aborting a listener by providing it a custom `AbortController`', () => {
   const listener = vi.fn()
   const emitter = new Emitter<{ greeting: [string] }>()
   const controller = new AbortController()
   emitter.earlyOnce('greeting', listener, { signal: controller.signal })
 
   controller.abort()
-  await emitter.emitAsPromise('greeting', 'John')
+  emitter.emit('greeting', 'John')
 
   expect(listener).not.toHaveBeenCalled()
 })
