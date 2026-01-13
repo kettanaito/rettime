ecf94a7d6ab02991c2aa2a7c4b9a2430835bfecf
Artem Zakharchenko <kettanaito@gmail.com>
2026-01-13 11:08:06 +0100

    test: add test for typeless event forwarding

--- test/event-forwarding.test.ts
+++ test/event-forwarding.test.ts
@@ -12,6 +12,32 @@ it('forwards event to another emitter', () => {
 
   const event = new TypedEvent('one', { data: 'hello' })
   expect(emitterOne.emit(event)).toBe(true)
-  expect(listener).toHaveBeenCalledTimes(1)
+  expect(listener).toHaveBeenCalledOnce(1)
   expect(listener).toHaveBeenCalledWith(event)
+})
+
+it('forwards all events via a typeless listener', () => {
+  const emitterOne = new Emitter<{
+    one: TypedEvent<string>
+    two: TypedEvent<number>
+  }>()
+  const emitterTwo = new Emitter<{
+    one: TypedEvent<string>
+    two: TypedEvent<number>
+  }>()
+
+  const listener = vi.fn()
+  emitterOne.on((event) => emitterTwo.emit(event))
+  emitterTwo.on(listener)
+
+  const firstEvent = new TypedEvent('one', { data: 'hello' })
+
+  expect(emitterOne.emit(firstEvent)).toBe(true)
+  expect(listener).toHaveBeenCalledOnce()
+  expect(listener).toHaveBeenCalledWith(firstEvent)
+
+  const secondEvent = new TypedEvent('two', { data: 123 })
+  expect(emitterOne.emit(secondEvent)).toBe(true)
+  expect(listener).toHaveBeenCalledTimes(2)
+  expect(listener).toHaveBeenCalledWith(secondEvent)
 })
